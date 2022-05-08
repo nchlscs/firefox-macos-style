ab6e6dc4acef580fe70aa088d588a44e11d05826
Nikolay Davydov <nik.davidov@gmail.com>
2022-05-08 13:06:33 +0400

    Disable new tab search

--- Source/user.js
+++ Source/user.js
@@ -7,5 +7,6 @@ user_pref("full-screen-api.macos-native-full-screen", true);
 user_pref("full-screen-api.warning.delay", -1);
 user_pref("full-screen-api.warning.timeout", 0);
 
+user_pref("browser.newtabpage.activity-stream.showSearch", false);
 user_pref("browser.toolbars.bookmarks.visibility", "never");
 user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[\"sync-button\"],\"nav-bar\":[\"sidebar-button\",\"back-button\",\"forward-button\",\"stop-reload-button\",\"customizableui-special-spring1\",\"home-button\",\"urlbar-container\",\"customizableui-special-spring2\",\"customizableui-special-spring3\",\"downloads-button\",\"new-tab-button\",\"_testpilot-containers-browser-action\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"alltabs-button\"],\"PersonalToolbar\":[\"import-button\",\"personal-bookmarks\"]},\"seen\":[],\"dirtyAreaCache\":[],\"currentVersion\":17,\"newElementCount\":14}");
